4ab8f77aeee3a3b345808188f713050edf8a6224
Phạm Trung Anh Dũng <73769984+phamtrunganhdung@users.noreply.github.com>
2024-06-05 21:22:12 +0700

    load data

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,170 @@
-import Image from "next/image";
+"use client";
+import { useEffect } from "react";
+import meetings from "./file/meetings.json";
+import users from "./file/users.json";
+
+interface Columns {
+  label: string;
+  dataIndex: string;
+  key: string;
+  render?: (value: any, record: Users, index: number) => React.ReactNode;
+  width?: number;
+  align?: string;
+}
+interface Users {
+  days: number;
+  email: string;
+  first_name: string;
+  gender: string;
+  id: number;
+  ip_address: string;
+  last_name: string;
+}
+
+interface Meetings {
+  end_day: number;
+  id: number;
+  room_id: number;
+  start_day: number;
+  user_id: number;
+}
 
 export default function Home() {
+  const columns: Columns[] = [
+    {
+      label: "ID",
+      dataIndex: "id",
+      key: "id",
+      align: "center",
+    },
+    {
+      label: "First Name",
+      dataIndex: "first_name",
+      key: "first_name",
+    },
+    {
+      label: "Last Name",
+      dataIndex: "last_name",
+      key: "last_name",
+    },
+    {
+      label: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      label: "Gender",
+      dataIndex: "gender",
+      key: "gender",
+      align: "center",
+    },
+    {
+      label: "Days",
+      dataIndex: "days",
+      key: "days",
+      align: "center",
+    },
+    {
+      label: "Meeting Days",
+      dataIndex: "meeting_days",
+      key: "meeting_days",
+      render: (_, r) => {
+        const lstMeetings: Meetings[] = meetings.filter(
+          (mt: Meetings) => mt.user_id === r.id
+        );
+        return (
+          <div className="grid grid-cols-3 gap-1">
+            {lstMeetings.map((mt: Meetings) => {
+              return (
+                <div
+                  className="rounded text-xs font-bold w-full px-1 py-1 bg-neutral-200 text-center"
+                  key={mt.id}
+                >{`${mt.start_day} -> ${mt.end_day}`}</div>
+              );
+            })}
+          </div>
+        );
+      },
+    },
+    {
+      label: "Days Without Meetings",
+      dataIndex: "days_without__meetings",
+      key: "days_without__meetings",
+      render: (_, r) =>
+        `${meetings.filter((mt: Meetings) => mt.user_id === r.id).length} days`,
+      align: "center",
+    },
+  ];
+  useEffect(() => {
+    console.log(meetings);
+    console.log(users);
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">
+          Get & Show data working days
+        </h1>
+        <div className="overflow-x-auto xl:overflow-x-visible overflow-y-auto h-[70vh]">
+          <table className="min-w-full bg-white text-neutral-800">
+            <thead className="sticky top-0 bg-gray-200">
+              <tr>
+                {columns.map((hd: Columns) => {
+                  return (
+                    <th
+                      key={hd.key}
+                      className={`py-2 px-4 bg-gray-200 text-left
+                      border-b
+                      ${
+                        hd.align === "left"
+                          ? "text-left"
+                          : hd.align === "right"
+                          ? "text-right"
+                          : hd.align === "center"
+                          ? "text-center"
+                          : "text-left"
+                      }
+                      `}
+                    >
+                      {hd.label}
+                    </th>
+                  );
+                })}
+              </tr>
+            </thead>
+            <tbody className="text-sm">
+              {users.map((rc: Users, index: number) => {
+                return (
+                  <tr key={rc.id}>
+                    {columns.map((cl: Columns) => {
+                      const value = rc[cl.dataIndex as keyof Users];
+                      return (
+                        <td
+                          key={cl.key}
+                          className={`
+                          border-b
+                          py-2 px-4
+                            ${
+                              cl.align === "left"
+                                ? "text-left"
+                                : cl.align === "right"
+                                ? "text-right"
+                                : cl.align === "center"
+                                ? "text-center"
+                                : "text-left"
+                            }
+                            `}
+                        >
+                          {cl.render ? cl.render(value, rc, index) : value}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
